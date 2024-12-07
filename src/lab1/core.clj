7c518ba6e9c639dd5aebd3212c097f6bcda6c9a8
karanreader <karanreaderer@gmail.com>
2024-12-07 10:07:01 +0700

    +

--- src/lab1/core.clj
+++ src/lab1/core.clj
@@ -1,61 +1,82 @@
 (ns lab1.core)
 
-(defn concatToEvery [c l]
-  "Adds c to every element in l if the element does not start with c."
-  (map (fn [item] (str c item))
-       (filter (fn [item] (not (.startsWith item c))) l)))
+ (defn concatToEvery-loop [c l]
+   "Concatenates c to every element in l if the element does not start with c, using recur."
+   (loop [acc '() new-l l]
+     (if (empty? new-l)
+       (reverse acc)
+       (let [head (first new-l)
+             tail (rest new-l)]
+         (if (.startsWith head c)
+           (recur acc tail)
+           (recur (conj acc (str c head)) tail))))))
 
-(defn step [l acc]
-  "Performs one step of concatenation for each character in l with every element in acc."
-  (reduce concat (map (fn [c] (concatToEvery c acc)) l)))
+ (defn foo-loop [l n]
+   (loop [acc l count n]
+     (cond (<= count 0) '()
+           (= count 1) acc
+           :else
+           (let [new-acc (loop [inner-acc '() rem l]
+                           (if (empty? rem)
+                             inner-acc
+                             (let [head (first rem) tail (rest rem)]
+                               (recur (concat inner-acc (concatToEvery-loop head acc)) tail))))]
+             (recur new-acc (dec count))))))
 
-(defn foo [l n]
-  "Generates a list of all strings of length n without consecutive repeating characters."
-  (loop [acc l, count n]
-    (cond
-      (<= count 0) '()
-      (= count 1) acc
-      :else (recur (step l acc) (dec count)))))
+ (defn concatToEvery-map [c l]
+   (map (fn [item] (str c item))
+        (filter (fn [item] (not (.startsWith item c))) l)))
 
-(defn my-square [x]
-  "Calculates the square of x."
-  (* x x))
+ (defn step [l acc]
+   (reduce concat (map (fn [c] (concatToEvery-map c acc)) l)))
 
-(defn my-map [f l]
-  "Applies function f to each element in list l and returns a new list."
-  (reduce
-   (fn [res el]
-     (concat res (list (f el))))
-   '() l))
+ (defn foo-map [l n]
+   (loop [acc l, count n]
+     (cond
+       (<= count 0) '()
+       (= count 1) acc
+       :else (recur (step l acc) (dec count)))))
 
-(defn my-filter [f l]
-  "Filters elements in list l based on the function f."
-  (reduce (fn [res el]
-            (if (f el)
-              (concat res (list el))
-              res))
-          '() l))
+ (defn my-square [x]
+   (* x x))
 
-(defn -main [& args]
-  (let [a '(1 2 3 4 5)
-        l '("a" "b" "c")]
-    
-    ;; Part 1.1 + 1.2
-    (println "\nFunction concatToEvery:")
-    (println (concatToEvery "a" '("a" "b" "c")))
+ (defn my-map [f l]
+   (reduce
+    (fn [res el]
+      (concat res (list (f el))))
+    '() l))
 
-    (println "\nFunction foo:")
-    (println "foo l 0:" (foo l 0)) ; '()
-    (println "foo l 1:" (foo l 1)) ; ("a" "b" "c")
-    (println "foo l 2:" (foo l 2)) ; ("ab" "ac" "ba" "bc" "ca" "cb")
-    (println "foo l 3:" (foo l 3))
+ (defn my-filter [f l]
+   (reduce (fn [res el]
+             (if (f el)
+               (concat res (list el))
+               res))
+           '() l))
 
-    ;; Part 1.3
-    (println "\nFunctions my-map and my-filter:")
-    (println "my-map my-square a:" (my-map my-square a))
-    (println "my-filter even? a:" (my-filter even? a))
+ (defn -main [& args]
+   (let [a '(1 2 3 4 5)
+         b '("Hello" "world")
+         l '("a" "b" "c")
+         lb '("a" "b" "c" "d")]
+     (println "Testing concatToEvery-loop:")
+     (println (concatToEvery-loop "a" '("a" "b" "c")))
 
-    ;; Part 1.4
-    (println "\nFunction foo2:")
-    (println "foo2 l 1:" (foo l 1))
-    (println "foo2 l 3:" (foo l 3))))
+     (println "\nTesting foo-loop:")
+     (println "foo-loop l 0:" (foo-loop l 0))
+     (println "foo-loop l 1:" (foo-loop l 1))
+     (println "foo-loop l 2:" (foo-loop l 2))
+     (println "foo-loop l 3:" (foo-loop l 3))
+
+     (println "\nTesting my-square:")
+     (println (my-square 10))
+
+     (println "\nTesting my-map:")
+     (println (my-map my-square a))
+
+     (println "\nTesting my-filter:")
+     (println (my-filter even? a))
+
+     (println "\nTesting foo-map:")
+     (println "foo-map l 1:" (foo-map l 1))
+     (println "foo-map l 3:" (foo-map l 3))))
+
